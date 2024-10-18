196a3326248520d20fd8c4663806571633847ddf
thumbert <tony.humberts@gmail.com>
2024-10-18 14:31:22 -0400

    nothing really

--- src/db/isone/mis/lib_mis.rs
+++ src/db/isone/mis/lib_mis.rs
@@ -1,26 +1,41 @@
-use std::fs::File;
+use std::{fs::File, path::Path};
 
-use jiff::{civil::Date, Timestamp};
-
-
+use jiff::{civil::Date, Timestamp, Zoned};
 
 struct MisReport {
     report_name: String,
-    account_id: String,
+    account_id: usize,
     report_date: Date,
     timestamp: Timestamp,
 }
 
 impl MisReport {
-
     /// Parse a filename to return an MisReport.
     /// SD_RTLOAD_000000003_2017060100_20190205151707.CSV
     fn from_filename(filename: &str) -> MisReport {
-        let name = "".to_string();
-        // let parts = filename.split(pat)
-        MisReport { report_name: name, account_id: (), report_date: (), timestamp: () }
-    } 
-
+        let path = Path::new(filename);
+        let mut parts: Vec<&str> = path
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .split("_")
+            .collect();
+        parts.reverse();
+        println!("{:?}", parts);
+        let zdt = Zoned::strptime("%Y%m%d%H%M%S%Z", format!("{}Z",parts[0]));
+        println!("{:?}", zdt);
+        let timestamp = parts[0].parse::<Timestamp>().unwrap();
+        let report_date = parts[1].to_string()[..10].parse::<Date>().unwrap();
+        let account_id = parts[2].parse::<usize>().unwrap();
+        let report_name = parts[3..].join("");
+        MisReport {
+            report_name,
+            account_id,
+            report_date,
+            timestamp,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -35,9 +50,12 @@ mod tests {
     fn from_filename() -> Result<(), Box<dyn Error>> {
         let filename = "SD_RTLOAD_000000003_2017060100_20190205151707.CSV";
         let report = MisReport::from_filename(filename);
-        assert_eq!(report.account_id, "000000003");
+        assert_eq!(report.account_id, 3);
         assert_eq!(report.report_date, "2017-06-01".parse::<Date>()?);
-        assert_eq!(report.timestamp, "2019-02-05T15:17:07Z".parse::<Timestamp>()?);
+        assert_eq!(
+            report.timestamp,
+            "2019-02-05T15:17:07Z".parse::<Timestamp>()?
+        );
         Ok(())
     }
 }
