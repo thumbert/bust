5b63e20fefea2ad1c4b142dd06468485e59bd919
thumbert <tony.humberts@gmail.com>
2024-10-20 18:23:05 -0400

    merge

--- src/db/isone/mis/lib_mis.rs
+++ src/db/isone/mis/lib_mis.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, path::Path};
 
-use jiff::{civil::Date, Timestamp, Zoned};
+use jiff::{civil::{Date, Time}, Timestamp, Zoned};
 
 struct MisReport {
     report_name: String,
@@ -8,6 +8,9 @@ struct MisReport {
     report_date: Date,
     timestamp: Timestamp,
 }
+
+#[derive(Debug, Clone)]
+struct MisReportError;
 
 impl MisReport {
     /// Parse a filename to return an MisReport.
@@ -22,13 +25,21 @@ impl MisReport {
             .split("_")
             .collect();
         parts.reverse();
-        println!("{:?}", parts);
-        let zdt = Zoned::strptime("%Y%m%d%H%M%S%Z", format!("{}Z",parts[0]));
-        println!("{:?}", zdt);
-        let timestamp = parts[0].parse::<Timestamp>().unwrap();
-        let report_date = parts[1].to_string()[..10].parse::<Date>().unwrap();
+
+        // timestamp is from parts[0]
+        let date = Date::strptime("%Y%m%d", parts[0].get(0..8).unwrap()).unwrap();
+        let time = Time::strptime("%H%M%S", parts[0].get(8..).unwrap()).unwrap();
+        let zdt = date
+            .at(time.hour(), time.minute(), time.second(), 0)
+            .intz("UTC")
+            .unwrap();
+        let timestamp = zdt.timestamp();
+
+        let report_date = parts[1].to_string()[..8].parse::<Date>().unwrap();
         let account_id = parts[2].parse::<usize>().unwrap();
-        let report_name = parts[3..].join("");
+        let rn: Vec<&str> = parts[3..].iter().copied().rev().collect();
+        let report_name = rn.join("_");
+
         MisReport {
             report_name,
             account_id,
@@ -37,6 +48,7 @@ impl MisReport {
         }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -50,6 +62,7 @@ mod tests {
     fn from_filename() -> Result<(), Box<dyn Error>> {
         let filename = "SD_RTLOAD_000000003_2017060100_20190205151707.CSV";
         let report = MisReport::from_filename(filename);
+        assert_eq!(report.report_name, "SD_RTLOAD".to_string());
         assert_eq!(report.account_id, 3);
         assert_eq!(report.report_date, "2017-06-01".parse::<Date>()?);
         assert_eq!(
