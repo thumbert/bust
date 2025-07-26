752665abd504ad95c4066c02c5f957a0e7aacce1
thumbert <tony.humberts@gmail.com>
2025-07-26 17:12:27 -0400

    buckets rewritten to use jiff

--- src/bucket.rs
+++ src/bucket.rs
@@ -6,138 +6,65 @@ use crate::holiday::*;
 
 pub trait BucketLike {
     fn name(self) -> String;
-    fn contains(datetime: &Zoned) -> bool;
+    fn contains(self, datetime: &Zoned) -> bool;
 }
 
-// some convenience definitions
-// pub const ATC: Bucket = Bucket::Atc;
+#[derive(Debug, PartialEq, Eq)]
+pub enum Bucket {
+    Atc,
+    B5x16,
+    B2x16H,
+    B7x8,
+    Offpeak,
+}
 
-// #[derive(Debug, PartialEq, Eq)]
-// pub enum Bucket {
-//     Atc,
-//     B5x16,
-//     B2x16H,
-//     B7x8,
-// }
-
-// fn parse(s: &str) -> Result<Bucket, ParseError> {
-//     match s.to_uppercase().as_str() {
-//         "FLAT" | "ATC" => Ok(Bucket::Atc),
-//         "5X16" | "PEAK" => Ok(Bucket::B5x16),
-//         "2X16H" => Ok(Bucket::B2x16H),
-//         "7X8" => Ok(Bucket::B7x8),
-//         _ => Err(ParseError),
-//     }
-// }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct ParseError;
 
-// pub struct Bucket<State> {
-//     state: std::marker::PhantomData<State>,
-// }
 
-impl BucketLike for BucketAtc {
-    fn name(self) -> String {
-        String::from("ATC")
-    }
-    fn contains(_: &Zoned) -> bool {
-        true
+impl FromStr for Bucket {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match parse_bucket(s) {
+            Ok(bucket) => Ok(bucket),
+            Err(_) => Err(format!("Failed parsing {} as an bucket", s)),
+        }
     }
 }
 
-impl BucketLike for Bucket5x16 {
-    fn name(self) -> String {
-        String::from("5x16")
-    }
-    fn contains(datetime: &Zoned) -> bool {
-        contains_5x16(datetime)
-    }
-}
-
-impl BucketLike for Bucket2x16H {
-    fn name(self) -> String {
-        String::from("2x16H")
-    }
-    fn contains(datetime: &Zoned) -> bool {
-        contains_2x16h(datetime)
+fn parse_bucket(s: &str) -> Result<Bucket, ParseError> {
+    match s.to_uppercase().as_str() {
+        "FLAT" | "ATC" => Ok(Bucket::Atc),
+        "5X16" | "PEAK" => Ok(Bucket::B5x16),
+        "2X16H" => Ok(Bucket::B2x16H),
+        "7X8" => Ok(Bucket::B7x8),
+        "OFFPEAK" => Ok(Bucket::Offpeak),
+        _ => Err(ParseError),
     }
 }
 
-impl BucketLike for Bucket7x8 {
+impl BucketLike for Bucket {
     fn name(self) -> String {
-        String::from("7x8")
+        match self {
+            Bucket::Atc => String::from("ATC"),
+            Bucket::B5x16 => String::from("5x16"),
+            Bucket::B2x16H => String::from("2x16H"),
+            Bucket::B7x8 => String::from("7x8"),
+            Bucket::Offpeak => String::from("Offpeak"),
+        }
     }
-    fn contains(datetime: &Zoned) -> bool {
-        contains_7x8(datetime)
+
+    fn contains(self, datetime: &Zoned) -> bool {
+        match self {
+            Bucket::Atc => true,
+            Bucket::B5x16 => contains_5x16(datetime),
+            Bucket::B2x16H => contains_2x16h(datetime),
+            Bucket::B7x8 => contains_7x8(datetime),
+            Bucket::Offpeak => !contains_5x16(datetime),
+        }
     }
 }
-
-
-// impl Bucket {
-//     pub fn atc() -> BucketAtc {
-//         BucketAtc {}
-//     }
-
-//     pub fn b5x16() -> Bucket5x16 {
-//         Bucket5x16 {}
-//     }
-
-//     pub fn b2x16h() -> Bucket2x16H {
-//         Bucket2x16H {}
-//     }
-
-//     pub fn b7x8() -> Bucket7x8 {
-//         Bucket7x8 {}
-//     }
-// }
-
-// impl FromStr for Bucket {
-//     type Err = String;
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         match parse_bucket(s) {
-//             Ok(bucket) => Ok(bucket),
-//             Err(_) => Err(format!("Failed parsing {} as an bucket", s)),
-//         }
-//     }
-// }
-
-// fn parse_bucket(s: &str) -> Result<BucketLike, ParseError> {
-//     match s.to_uppercase().as_str() {
-//         "FLAT" | "ATC" => Ok(Bucket::atc()),
-//         "5X16" | "PEAK" => Ok(Bucket::B5x16),
-//         "2X16H" => Ok(Bucket::B2x16H),
-//         "7X8" => Ok(Bucket::B7x8),
-//         _ => Err(ParseError),
-//     }
-// }
-
-
-
-struct BucketAtc;
-struct Bucket5x16;
-struct Bucket2x16H {}
-pub struct Bucket7x8 {}
-
-// impl BucketLike for Bucket {
-//     fn name(self) -> String {
-//         match self {
-//             Bucket::Atc => String::from("ATC"),
-//             Bucket::B5x16 => String::from("5x16"),
-//             Bucket::B2x16H => String::from("2x16H"),
-//             Bucket::B7x8 => String::from("7x8"),
-//         }
-//     }
-
-//     fn contains(self, datetime: &Zoned) -> bool {
-//         match self {
-//             Bucket::Atc => true,
-//             Bucket::B5x16 => contains_5x16(datetime),
-//             Bucket::B2x16H => contains_2x16h(datetime),
-//             Bucket::B7x8 => contains_7x8(datetime),
-//         }
-//     }
-// }
 
 fn contains_5x16(dt: &Zoned) -> bool {
     if dt.weekday() == Weekday::Saturday && dt.weekday() == Weekday::Sunday {
@@ -170,7 +97,7 @@ mod tests {
 
     use jiff::civil::date;
 
-    use crate::{bucket::{self, *}, interval::*};
+    use crate::{bucket::*};
 
     #[test]
     fn test_bucket_atc() {
@@ -178,7 +105,7 @@ mod tests {
             .at(0, 0, 0, 0)
             .in_tz("America/New_York")
             .unwrap();
-        assert!(bucket::BucketAtc::contains(&dt));
+        assert!(Bucket::Atc.contains(&dt));
         // assert!(ATC.contains(&dt));
         // assert_eq!(parse("Flat"), Ok(ATC));
     }
