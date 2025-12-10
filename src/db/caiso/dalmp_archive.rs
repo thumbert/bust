dd22d0b8f7040c106242bd4f3227053d988dd3b9
thumbert <tony.humberts@gmail.com>
2025-12-10 07:52:20 -0500

    merging

---
+++ src/db/caiso/dalmp_archive.rs
@@ -0,0 +1,180 @@
+use jiff::civil::Date;
+use jiff::Zoned;
+use log::{error, info};
+use rust_decimal::Decimal;
+use std::error::Error;
+use std::path::Path;
+use std::process::Command;
+
+use crate::interval::month::Month;
+
+#[derive(Debug, PartialEq)]
+pub struct Row {
+    hour_beginning: Zoned,
+    ptid: u32,
+    lmp: Decimal,
+    mcc: Decimal,
+    mlc: Decimal,
+}
+
+#[derive(Clone)]
+pub struct CaisoDaLmpArchive {
+    pub base_dir: String,
+    pub duckdb_path: String,
+}
+
+impl CaisoDaLmpArchive {
+    /// Return the json filename for the day.  Does not check if the file exists.  
+    pub fn filename(&self, date: &Date) -> String {
+        self.base_dir.to_owned()
+            + "/Raw/"
+            + &date.year().to_string()
+            + "/WW_DALMP_ISO_"
+            + &date.strftime("%Y%m%d").to_string()
+            + ".json"
+    }
+
+    /// Upload one month to DuckDB.
+    /// Assumes all json.gz file exists for DA and RT.  Skips the day if it doesn't exist.
+    ///  
+    pub fn update_duckdb(&self, month: &Month) -> Result<(), Box<dyn Error>> {
+        info!(
+            "inserting daily DALMP hourly price files for month {} ...",
+            month
+        );
+
+        let sql = format!(
+            r#"
+CREATE TABLE IF NOT EXISTS da_lmp (
+    hour_beginning TIMESTAMPTZ NOT NULL,
+    ptid UINTEGER NOT NULL,
+    lmp DECIMAL(9,4) NOT NULL,
+    mcc DECIMAL(9,4) NOT NULL,
+    mcl DECIMAL(9,4) NOT NULL,
+);
+
+CREATE TEMPORARY TABLE tmp
+AS
+    SELECT 
+        BeginDate::TIMESTAMPTZ AS hour_beginning,
+        "@LocId"::UINTEGER AS ptid,
+        LmpTotal::DECIMAL(9,4) AS "lmp",
+        CongestionComponent::DECIMAL(9,4) AS "mcc",
+        LossComponent::DECIMAL(9,4) AS "mcl" 
+    FROM (
+        SELECT DISTINCT BeginDate, "@LocId", LmpTotal, CongestionComponent, LossComponent FROM (
+            SELECT unnest(HourlyLmps.HourlyLmp, recursive := true)
+            FROM read_json('{}/Raw/{}/WW_DALMP_ISO_{}*.json.gz')
+        )
+    )
+    ORDER BY hour_beginning, ptid
+;
+
+INSERT INTO da_lmp
+(SELECT * FROM tmp 
+WHERE NOT EXISTS (
+    SELECT * FROM da_lmp d
+    WHERE d.hour_beginning = tmp.hour_beginning
+    AND d.ptid = tmp.ptid
+    )
+)
+ORDER BY hour_beginning, ptid;
+"#,
+            self.base_dir,
+            month.start_date().year(),
+            month.start_date().strftime("%Y%m"),
+        );
+        // println!("{}", sql);
+
+        let output = Command::new("duckdb")
+            .arg("-c")
+            .arg(&sql)
+            .arg(&self.duckdb_path)
+            .output()
+            .expect("Failed to invoke duckdb command");
+
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        if output.status.success() {
+            info!("{}", stdout);
+            info!("done");
+        } else {
+            error!("Failed to update duckdb for month {}: {}", month, stderr);
+        }
+
+        Ok(())
+    }
+
+    /// Data is usually published before 13:30 every day
+    pub fn download_file(&self, date: Date) -> Result<(), Box<dyn Error>> {
+        let yyyymmdd = date.strftime("%Y%m%d");
+        super::lib_isoexpress::download_file(
+            format!(
+                "https://webservices.iso-ne.com/api/v1.1/hourlylmp/da/final/day/{}",
+                yyyymmdd
+            ),
+            true,
+            Some("application/json".to_string()),
+            Path::new(&self.filename(&date)),
+            true,
+        )
+    }
+
+    /// Look for missing days
+    pub fn download_missing_days(&self, month: Month) -> Result<(), Box<dyn Error>> {
+        let mut last = Zoned::now().date();
+        if Zoned::now().hour() > 13 {
+            last = last.tomorrow()?;
+        }
+        for day in month.days() {
+            if day > last {
+                continue;
+            }
+            let fname = format!("{}.gz", self.filename(&day));
+            if !Path::new(&fname).exists() {
+                info!("Working on {}", day);
+                self.download_file(day)?;
+                info!("  downloaded file for {}", day);
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use jiff::civil::date;
+    use log::info;
+    use std::{error::Error, path::Path};
+
+    use crate::{db::prod_db::ProdDb, interval::month::month};
+
+    #[ignore]
+    #[test]
+    fn update_db() -> Result<(), Box<dyn Error>> {
+        let _ = env_logger::builder()
+            .filter_level(log::LevelFilter::Info)
+            .is_test(true)
+            .try_init();
+        dotenvy::from_path(Path::new(".env/test.env")).unwrap();
+        let archive = ProdDb::isone_dalmp();
+
+        let months = month(2022, 1).up_to(month(2022, 2));
+        for month in months.unwrap() {
+            info!("Working on month {}", month);
+            archive.download_missing_days(month)?;
+            archive.update_duckdb(&month)?;
+        }
+        Ok(())
+    }
+
+    #[ignore]
+    #[test]
+    fn download_file() -> Result<(), Box<dyn Error>> {
+        dotenvy::from_path(Path::new(".env/test.env")).unwrap();
+        let archive = ProdDb::isone_dalmp();
+        archive.download_file(date(2025, 7, 1))?;
+        Ok(())
+    }
+}
